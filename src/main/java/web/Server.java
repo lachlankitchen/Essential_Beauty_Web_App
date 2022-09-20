27163bb637402fb21c9e0b69aad0f24518c0872b
Lachlan Kitchen <kitla230@student.otago.ac.nz>
2022-09-20 18:10:53 +1200

    Add product objects upon Server construction for testing purposes

--- src/main/java/web/Server.java
+++ src/main/java/web/Server.java
@@ -34,16 +34,16 @@ public class Server extends Jooby {
         dao.saveProduct(new Product("HSM79", "Hot Stone Massage", "60 Minute hand massage", "Massage", new BigDecimal("75.0"), new BigDecimal("10")));
         dao.saveProduct(new Product("BT7123", "Back Exfoliation", "30 Minute Back blissful treatment", "Massage", new BigDecimal("70.0"), new BigDecimal("8")));
         dao.saveProduct(new Product("FBM724", "Fully Body Massage", "60 Minute deluxe massage", "Massage", new BigDecimal("60.0"), new BigDecimal("2")));
-        dao.saveProduct(new Product("UPW498", "Upper Lip Wax", "60 Minute deluxe massage", "Massage", new BigDecimal("10.0"), new BigDecimal("1")));
-        dao.saveProduct(new Product("FLW912", "Full Leg Wax", "60 Minute deluxe massage", "Massage", new BigDecimal("42.0"), new BigDecimal("9")));
-        dao.saveProduct(new Product("TQW438", "Three Quarter Wax", "60 Minute deluxe massage", "Massage", new BigDecimal("35.0"), new BigDecimal("3")));
-        dao.saveProduct(new Product("CW921", "Chin Wax", "60 Minute deluxe massage", "Massage", new BigDecimal("10.0"), new BigDecimal("7")));
-        dao.saveProduct(new Product("EF581", "Express Facial", "60 Minute deluxe massage", "Massage", new BigDecimal("60.0"), new BigDecimal("5")));
-        dao.saveProduct(new Product("TW913", "The Works", "60 Minute deluxe massage", "Massage", new BigDecimal("60.0"), new BigDecimal("6")));
-        dao.saveProduct(new Product("DF658", "Deluxe Facial", "60 Minute deluxe massage", "Massage", new BigDecimal("75.0"), new BigDecimal("8")));
-        dao.saveProduct(new Product("M581", "Microdermabrasion", "60 Minute deluxe massage", "Massage", new BigDecimal("65.0"), new BigDecimal("5")));
-        dao.saveProduct(new Product("IMP581", "Indulge Me Pack", "60 Minute deluxe massage", "Massage", new BigDecimal("90.0"), new BigDecimal("1")));
-        dao.saveProduct(new Product("ET913", "Eye Trio", "60 Minute deluxe massage", "Massage", new BigDecimal("34.0"), new BigDecimal("10")));
+        dao.saveProduct(new Product("UPW498", "Upper Lip Wax", "60 Minute deluxe massage", "Wax", new BigDecimal("10.0"), new BigDecimal("1")));
+        dao.saveProduct(new Product("FLW912", "Full Leg Wax", "60 Minute deluxe massage", "Wax", new BigDecimal("42.0"), new BigDecimal("9")));
+        dao.saveProduct(new Product("TQW438", "Three Quarter Wax", "60 Minute deluxe massage", "Wax", new BigDecimal("35.0"), new BigDecimal("3")));
+        dao.saveProduct(new Product("CW921", "Chin Wax", "60 Minute deluxe massage", "Wax", new BigDecimal("10.0"), new BigDecimal("7")));
+        dao.saveProduct(new Product("EF581", "Express Facial", "60 Minute deluxe massage", "Facial", new BigDecimal("60.0"), new BigDecimal("5")));
+        dao.saveProduct(new Product("TW913", "The Works", "60 Minute deluxe massage", "Other", new BigDecimal("60.0"), new BigDecimal("6")));
+        dao.saveProduct(new Product("DF658", "Deluxe Facial", "60 Minute deluxe massage", "Facial", new BigDecimal("75.0"), new BigDecimal("8")));
+        dao.saveProduct(new Product("M581", "Microdermabrasion", "60 Minute deluxe massage", "Other", new BigDecimal("65.0"), new BigDecimal("5")));
+        dao.saveProduct(new Product("IMP581", "Indulge Me Pack", "60 Minute deluxe massage", "Other", new BigDecimal("90.0"), new BigDecimal("1")));
+        dao.saveProduct(new Product("ET913", "Eye Trio", "60 Minute deluxe massage", "Other", new BigDecimal("34.0"), new BigDecimal("10")));
         
         System.out.println("\nStarting Server.");
         new Server().start();

8cf1be6b00e01aa02524303d79e5db6803e98024
Lachlan Kitchen <kitla230@student.otago.ac.nz>
2022-09-20 18:08:56 +1200

    Add Sale and SaleItem tables to schema

--- src/main/java/dao/schema.sql
+++ src/main/java/dao/schema.sql
@@ -1,21 +1,38 @@
 CREATE TABLE IF NOT EXISTS PRODUCT (
-PRODUCTID varchar(10) unique,
-NAME varchar(50) not null,
-DESCRIPTION varchar(200) not null,
-CATEGORY varchar(50) not null,
-LISTPRICE numeric(5,2) not null,
-QUANTITYINSTOCK double not null,
-constraint product_PK primary key (PRODUCTID)
+    PRODUCTID varchar(10) unique,
+    NAME varchar(50) not null,
+    DESCRIPTION varchar(200) not null,
+    CATEGORY varchar(50) not null,
+    LISTPRICE numeric(5,2) not null,
+    QUANTITYINSTOCK double not null,
+    constraint product_PK primary key (PRODUCTID)
+);
+
+CREATE TABLE IF NOT EXISTS CUSTOMER (
+    Customer_ID INTEGER AUTO_INCREMENT (1000) unique,
+    USERNAME varchar(50) unique not null,
+    FIRSTNAME varchar(50) not null,
+    SURNAME varchar(50) not null,
+    PASSWORD varchar(50) not null,
+    EMAILADDRESS varchar(50) not null,
+    SHIPPINGADDRESS varchar(50) not null,
+    constraint customer_PK primary key (Customer_ID)
 );
 
 
-CREATE TABLE IF NOT EXISTS CUSTOMER (
-Customer_ID INTEGER AUTO_INCREMENT (1000) unique,
-USERNAME varchar(50) unique not null,
-FIRSTNAME varchar(50) not null,
-SURNAME varchar(50) not null,
-PASSWORD varchar(50) not null,
-EMAILADDRESS varchar(50) not null,
-SHIPPINGADDRESS varchar(50) not null,
-constraint customer_PK primary key (Customer_ID)
+CREATE TABLE IF NOT EXISTS SALE (
+    SALE_ID INTEGER AUTO_INCREMENT (1000),
+    SALE_DATE DATE not null,
+    Customer_ID INTEGER not null,
+    STATUS varchar(10) not null,
+    constraint sale_PK primary key (SALE_ID),
+    constraint sale_FK foreign key (Customer_ID) references CUSTOMER
+);
+
+CREATE TABLE IF NOT EXISTS SALE_ITEM (
+    QUANTITYPURCHASED integer not null,
+    SALEPRICE numeric(5,2) not null,
+    constraint sale_item_PK primary key (Customer_ID),
+    constraint saleItem_sale_FK foreign key (SALE_ID) references SALE,
+    constraint saleItem_product_FK foreign key (PRODUCTID) references PRODUCT
 );
